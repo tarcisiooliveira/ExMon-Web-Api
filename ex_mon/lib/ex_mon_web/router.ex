a3f20bb0d6987111add0d4482b7be98ee08960cc
Tarcisio Oliveira <tarcisio@ymail.com>
2020-12-11 21:36:06 -0300

    Refactor and forward
    Refatorando o código, seguindo em frente,  erro entendido e superado.

---
+++ ex_mon/lib/ex_mon_web/router.ex
@@ -0,0 +1,36 @@
+defmodule ExMonWeb.Router do
+  use ExMonWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", ExMonWeb do
+    pipe_through :api
+    # resources "/trainers", TrainersController, only: [:create, :show, :delete, :update]
+    post "/trainers", TrainersController, :create
+    post "/trainers/signin", TrainersController, :sign_in
+    # get "/pokemons/:name", PokemonsController, :show
+  end
+
+  scope "/", ExMonWeb do
+    pipe_through :api
+    get "/", WelcomeController, :index
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+      live_dashboard "/dashboard", metrics: ExMonWeb.Telemetry
+    end
+  end
+end
--- lib/ex_mon_web/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule ExMonWeb.Repo do
-  use Ecto.Repo,
-    otp_app: :ex_mon_web,
-    adapter: Ecto.Adapters.Postgres
-end
